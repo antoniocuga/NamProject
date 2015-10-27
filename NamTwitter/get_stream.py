70ede49dd7baf86bb9e77c0cf490b03ba6de8313
Antonio Cucho <antoniocuga@gmail.com>
2015-10-26 18:05:09 -0600

    add support with Flask.

--- NamTwitter/get_stream.py
+++ NamTwitter/get_stream.py
@@ -2,8 +2,27 @@
 # -*- coding: utf-8 -*-
 
 from twython import Twython
+from flask import Flask, Response, json
 
 
+#Flask app
+app = Flask(__name__)
+
+
+@app.route("/")
+def init():
+
+	#Get tweets by term
+	results = getStream('#Ñam', 1000)
+
+	#Returns a json format
+	json = formatStream(results)	
+
+	#Prin values in json, ready to use in javascript
+	return Response(json,  mimetype='application/json')
+
+
+#Setup to get Tweets
 def setupStream():
 
 	t = Twython(app_key='', 
@@ -13,32 +32,26 @@ def setupStream():
 	return t
 
 
+#Returns tweets, we can add more functions, filters, etc.
 def getStream(term, count):
 	t = setupStream()
 	result = t.search(q=term, count=count)
 	return result
 
 
+#Format and values to return
 def formatStream(results):
 
-	data = []
-	tweets = results['statuses']
-
-	for tweet in tweets:
-  		data.append([tweet['id_str'], tweet['text']])
-
-	return data
+	tweets = []	
+	for t in results['statuses']:
+		tweets.append(t)
+  	return json.dumps(tweets)
 
 
+#Init the app
 if __name__ == '__main__':
 
-	#Get tweets by term
-	results = getStream('#Ñam', 1000)
+	#Active debug
+	app.debug = True
 
-	#Format results (JSON, CSV). Now is list
-	data = formatStream(results)
-
-	#Print results
-	print data
-
-
+	app.run()
